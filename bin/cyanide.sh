bc2f5623bd8c10f9150917777495a8f7237ed841
lidstah <lidstah@orange.fr>
2015-03-24 23:25:13 +0100

    modification cyanide downloader

--- bin/cyanide.sh
+++ bin/cyanide.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 touch /tmp/cyanide
 touch /tmp/cyanide.png
-CYANIDE_URL="http://www.explosm.net/comics/new/"
+CYANIDE_URL="http://www.explosm.net/comics/latest/"
 wget -q -O "/tmp/cyanide" $CYANIDE_URL
 # grep db\/files\Comics /tmp/cyanide # DEBUG
-IMAGE_LOCATION=`grep db\/files\/Comics /tmp/cyanide | sed -n 's/\(.*\)\(http:\/\/.*png\)\(.*\)/\2/p'`
+#IMAGE_LOCATION=`grep db\/files\/Comics /tmp/cyanide | sed -n 's/\(.*\)\(http:\/\/.*png\)\(.*\)/\2/p'`
+IMAGE_LOCATION=`grep "og:image" /tmp/cyanide | cut -d" " -f3 | cut -d'"' -f2`
 wget -q -O "/tmp/cyanide.png" $IMAGE_LOCATION
 echo $IMAGE_LOCATION # DEBUG
 feh /tmp/cyanide.png
